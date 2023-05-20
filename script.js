f5af441812f2bf52adaf25928e31e551b7de3032
Chinonye Ekpendu <alexekpendu001@gmail.com>
2023-05-20 15:56:43 +0100

    data-carousel-button value (prev & next)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
-    button.addEventListener
+    button.addEventListener("click", ()=> {
+        
+    })
 })
