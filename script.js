b781834285461a4d1ca33c280723f9fe55fe689e
Chinonye Ekpendu <alexekpendu001@gmail.com>
2023-05-21 22:08:56 +0100

    mod for buttons.forEach

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
     button.addEventListener("click", ()=> {
-        
+        const offset = button.dataset.carouselButton
     })
 })
