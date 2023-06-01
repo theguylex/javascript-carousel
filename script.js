053cce90eb4757e815d69d3691078f227626a72c
Chinonye Ekpendu <alexekpendu001@gmail.com>
2023-06-01 19:56:14 +0100

    if newIndex is >= slides.children.length

--- script.js
+++ script.js
@@ -8,5 +8,6 @@ buttons.forEach(button => {
         const activeSlide = slides.querySelector("[data-active]")
         let newIndex = [...slides.children].indexOf(activeSlide) + offset
         if (newIndex < 0) newIndex - slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
     })
 })
